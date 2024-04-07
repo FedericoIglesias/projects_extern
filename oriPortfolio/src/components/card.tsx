45a9fd53ed349e76fb03325ffda9c96798034df9
FedericoIglesias <igl.fede@gmail.com>
2024-04-07 22:40:49 +0200

    doneCardRepo

--- oriPortfolio/src/components/card.tsx
+++ oriPortfolio/src/components/card.tsx
@@ -1,23 +1,41 @@
 import { Repo } from "../vite-env";
-import styled from "styled-components";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-const CardArticle = styled.article`
-background-color: white ;
-width: 300px;
-height: 200px;
-`;
+const CardProject = (repo) => {
+  const repositorio: Repo = repo.repositorio;
+  const date: string = repositorio.created_at.substring(0,10);
 
-const CardProject = (repo ) => {
-  const repositorio: Repo = repo.repositorio
-  console.log(repositorio);
-  
   return (
-    <CardArticle key={repositorio.id}>
-      <p>{repositorio.name}</p>
-      {/* <p>{repositorio.html_url}</p> */}
-      <p>{repositorio.created_at}</p>
-      <p>{repositorio.description}</p>
-    </CardArticle>
+    <Card sx={{ width: 400 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {repositorio.name}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Created {date}
+        </Typography>
+
+        <Typography variant="body2">{repositorio.description}</Typography>
+      </CardContent>
+      <CardActions>
+        <a href={repositorio.html_url} target="_blank">
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<GitHubIcon />}
+          >
+            Repo
+          </Button>
+        </a>
+      </CardActions>
+    </Card>
   );
 };
 
--- oriPortfolio/src/components/projects.tsx
+++ oriPortfolio/src/components/projects.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import CardProject from "./card";
